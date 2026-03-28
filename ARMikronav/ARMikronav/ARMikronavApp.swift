a507fb008c4b7c57b157bfe1d112c53065e16e93
JSss10 <jessica.schneiter@bluewin.ch>
2026-03-28 15:51:21 +0100

    chore: create Xcode project with SwiftUI lifecycle

---
+++ ARMikronav/ARMikronav/ARMikronavApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARMikronavApp.swift
+//  ARMikronav
+//
+//  Created by Jessica Schneiter on 28.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct ARMikronavApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
